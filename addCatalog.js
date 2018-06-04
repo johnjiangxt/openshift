70ef6c7d2b4f5a47a8241d75b29246da2acad442
root <root@localhost.localdomain>
2018-06-04 13:24:07 +0800

    modify to ai catalog to technology catalog

--- addCatalog.js
+++ addCatalog.js
@@ -1,18 +1,17 @@
 // Find the Languages category.
 var category = _.find(window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES,
-                      { id: 'ai' });
+                      { id: 'technologies' });
 // Add Go as a new subcategory under Languages.
 category.subCategories.splice(2,0,{ // Insert at the third spot.
   // Required. Must be unique.
-  id: "tensorflow",
+  id: "ai",
   // Required.
-  label: "Tensorflow",
+  label: "AI",
   // Optional. If specified, defines a unique icon for this item.
   icon: "icon-go-gopher",
   // Required. Items matching any tag will appear in this subcategory.
   tags: [
-    "tensorflow",
-    "TF"
+    "ai"
   ]
 });
 
